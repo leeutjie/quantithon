1faf159e10469d049842246f51f48400e9160267
Louis Becker <louis.becker@prescient.co.za>
2023-10-06 11:05:08 +0200

    added bonds

--- R/code/bb_etl.R
+++ R/code/bb_etl.R
@@ -1,15 +1,22 @@
 pimR::tool()
 
 ret <-
-  pimR::fetch("SPX Index", "RT116", min=F) %>%
+  pimR::fetch(c("SPX Index", "LUATTRUU Index"),
+              "RT116",
+              min = F) %>%
+  mutate(
+    entity = stringr::str_replace(entity, " Index", ""),
+    item = "RETURN"
+  ) %>%
+  unite(item, c(entity, item)) %>%
   select(date = datestamp, item, value) %>%
-  mutate(item = "SPX_RETURN") %>%
   pivot_wider(names_from = "item")
 
 data <- read_csv("../data/quantathon_hannah.csv")
 
 data %>%
   left_join(ret, by = "date") %>%
-  select(-SPX_PX_LAST) %>%
+  select(-c(SPX_PX_LAST, LUATTRUU_PX_LAST)) %>%
   relocate(SPX_RETURN, .before = SPX_PX_BID) %>%
   write_rds("data.rds")
+
